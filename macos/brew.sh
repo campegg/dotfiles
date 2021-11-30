45fccb9e79c78605afacf6fc9209c22c48db5e42
Cam Pegg <cam@campegg.com>
2021-11-30 16:32:42 -0500

    Update brew config

--- macos/brew.sh
+++ macos/brew.sh
@@ -12,29 +12,18 @@ if hash brew 2>/dev/null; then
 else
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-    # Install other useful binaries.
-    echo "Installing useful tools..."
-    brew install curl
-    brew install git
-    brew install nano
-    brew install openssl
-    brew install python3
-    brew install ruby
-    brew install sqlite3
-    brew install ssh-copy-id
-    brew install wget
-
-
-    # Install apps
-    echo "Installing apps..."
-    brew install cask 'appcleaner'
-    brew install cask 'db-browser-for-sqlite'
-    brew install cask 'imageoptim'
-    brew install cask 'launchbar'
-    brew install cask 'subethaedit'
-    brew install cask 'the-unarchiver'
 fi
+
+
+# Install other useful binaries.
+echo "Installing useful tools..."
+brew install curl nano openssl python3 ruby sqlite3 ssh-copy-id wget
+
+
+# Install apps
+echo "Installing apps..."
+brew install cask 'appcleaner' 'db-browser-for-sqlite' 'imageoptim' 'launchbar' 'subethaedit' 'the-unarchiver'
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
